49e8f469fc59c29ebeb5104e2b93a807732db7e6
The_Statue <dylan.saurbrey@ymail.com>
2022-07-20 21:31:49 +0930

    Added handling multiple fingers for joystick, added camera follower maintaining 45* angle

--- ChaosandDeduction/Assets/Scripts/Joystick.cs
+++ ChaosandDeduction/Assets/Scripts/Joystick.cs
@@ -9,17 +9,20 @@ public class Joystick : MonoBehaviour
 {
     public RectTransform knob;
     RectTransform backPlate;
-    public float magnitudeMultiplier = 1;
+    public float rimMultiplier = 1;
     public Canvas canvas;
+
+    Finger legalFinger;
 
     public Vector2 inputVector
     {
-        get { return knob.anchoredPosition / (backPlate.sizeDelta.x * magnitudeMultiplier); }
+        get { return knob.anchoredPosition / (backPlate.sizeDelta.x * rimMultiplier); }
     }
     // Start is called before the first frame update
     void Start()
     {
         EnhancedTouchSupport.Enable();
+        UnityEngine.InputSystem.EnhancedTouch.Touch.onFingerDown += OnFingerDown;
         UnityEngine.InputSystem.EnhancedTouch.Touch.onFingerMove += OnMoveFinger;
         UnityEngine.InputSystem.EnhancedTouch.Touch.onFingerUp += OnFingerUp;
 
@@ -32,9 +35,28 @@ public class Joystick : MonoBehaviour
 
     }
 
+    public void OnFingerDown(Finger finger)
+    {
+        if (legalFinger != null) //if joystick already engaged, ignore extra fingers
+            return;
+
+
+        Vector2 fingerPos = finger.screenPosition;
+
+        if (canvas != null) //annoying screen scale fixing, god this took forever to find
+            fingerPos /= canvas.scaleFactor;
+
+        if ((fingerPos - backPlate.anchoredPosition - (backPlate.sizeDelta / 2)).magnitude > backPlate.sizeDelta.x * rimMultiplier * 2)
+            return;
+
+        legalFinger = finger;
+        OnMoveFinger(finger); //run it once at the start incase no movement on finger
+    }
+
+
     public void OnMoveFinger(Finger finger)
     {
-        if ((finger.screenPosition - backPlate.anchoredPosition - (backPlate.sizeDelta / 2)).magnitude > backPlate.sizeDelta.x * magnitudeMultiplier * 2)
+        if (!finger.Equals(legalFinger)) //only move joystick using the finger that triggerd it
             return;
 
         Vector2 fingerPos = finger.screenPosition;
@@ -44,12 +66,17 @@ public class Joystick : MonoBehaviour
 
         knob.anchoredPosition = fingerPos - backPlate.anchoredPosition - (backPlate.sizeDelta / 2);
 
-        if (knob.anchoredPosition.magnitude > backPlate.sizeDelta.x * magnitudeMultiplier)
-            knob.anchoredPosition = knob.anchoredPosition.normalized * backPlate.sizeDelta.x * magnitudeMultiplier;
+        if (knob.anchoredPosition.magnitude > backPlate.sizeDelta.x * rimMultiplier) //cap the joystick to the rim
+            knob.anchoredPosition = knob.anchoredPosition.normalized * backPlate.sizeDelta.x * rimMultiplier;
     }
 
     public void OnFingerUp(Finger finger)
     {
+        if (!finger.Equals(legalFinger)) //only reset if its the correct finger going up
+            return;
+
         knob.anchoredPosition = new Vector2(0, 0);
+
+        legalFinger = null;
     }
 }
