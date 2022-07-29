87263817be4ea184ae741340d890c5352a337f8e
Dylan Saurbrey <dylan.saurbrey@ymail.com>
2022-07-29 22:17:17 +0930

    Added script to bind the interact UI button on runtime for multiplayer reasons

---
+++ ChaosandDeduction/Assets/Scripts/Character/BindInteract.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BindInteract : MonoBehaviour
+{
+    GameObject player;
+    public Button button;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject temp = PlayerManager.Instance.localPlayer;
+        if (temp != null && (temp != player || player == null))
+        {
+            player = PlayerManager.Instance.localPlayer;
+            button.onClick.RemoveAllListeners();
+            button.onClick.AddListener(player.GetComponent<CharacterInteraction>().Interact);
+        }
+    }
+}
