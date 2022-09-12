94de7a7ec511eb399ab93625236b43f57878e039
AIE\s210559 <s210559@students.aie.edu.au>
2022-09-12 09:50:28 +0930

    move buttonfix build stuff

--- ChaosandDeduction/Assets/Scripts/Autojoin.cs
+++ ChaosandDeduction/Assets/Scripts/Autojoin.cs
@@ -2,8 +2,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#if UNITY_EDITOR
 using UnityEditor;
 using ParrelSync;
+#endif
 using Mirror;
 using Mirror.Discovery;
 
