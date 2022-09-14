62ccb0274174a1c3df04631ce35500c23ab1a35e
Ivorygriffin <s210559@students.aie.edu.au>
2022-09-14 13:20:20 +0930

    random

--- ChaosandDeduction/Assets/Sprites/villagerwalk.cs
+++ ChaosandDeduction/Assets/Sprites/villagerwalk.cs
@@ -22,84 +22,121 @@ public class villagerwalk : MonoBehaviour
     public bool ReachedEnd { get; private set; }
     public bool ReachedStart { get; private set; }
 
-    private int currentIndex = 0;
-
+    public int currentIndex = 0;
+    public bool runOnce;
+    public bool hasRotated;
 
     void Start()
     {
+        transform.position = paths[currentIndex].transform.position;
         if (spawnAtStart)
         {
-            transform.position = GetCurrentTransform().position;
+            //transform.position = GetCurrentTransform().position;
         }
     }
     private void Update()
     {
         Move();
-        GetCurrentTransform();
-        CheckPath(transform);
+        //GetCurrentTransform();
+        //CheckPath(transform);
     }
 
     private void Move()
     {
-        transform.position = Vector2.MoveTowards(transform.position, paths[currentIndex].transform.position, moveSpeed * Time.deltaTime);
+        if(currentIndex <= paths.Length -1 && ReachedEnd == false)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, paths[currentIndex].transform.position, moveSpeed * Time.deltaTime);
+        }
+        if(transform.position == paths[0].transform.position && runOnce == true && hasRotated ==false)
+        {
+            transform.Rotate(0, 180, 0);
+            hasRotated = true;
+        }
+       if (transform.position == paths[currentIndex].transform.position && ReachedEnd == false)
+        {
+            currentIndex += 1;
+            hasRotated = false;
+            runOnce = true;
+        }
+       if(currentIndex == paths.Length)
+        {
+            ReachedEnd = true;
+            //idle animation switch here
+            //wait for 2 seconds
+            transform.Rotate(0, 180, 0);
+            //swich animation back
+            currentIndex -= 1;
+        }
+        if (ReachedEnd)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, paths[currentIndex].transform.position, moveSpeed * Time.deltaTime);
+        }
+        if(ReachedEnd && transform.position == paths[currentIndex].transform.position)
+        {
+            currentIndex -= 1;
+        }
+        if (currentIndex == 0)
+        {
+            ReachedEnd = false;
+        }
     }
     
     
     
     
-    public Transform GetCurrentTransform()
-    {
-        return paths[currentIndex];
-    }
+    //public Transform GetCurrentTransform()
+    //{
+    //    return paths[currentIndex];
+    //}
 
-    public void CheckPath(Transform other)
-    {
-        Transform currentTransform = GetCurrentTransform();
-        int range = (int)Vector2.Distance(currentTransform.position, other.position);
-        if (range == 0)
-        {
-            NextPath();
-        }
-    }
-    private void NextPath()
-    {
-        int nextIndex = direction == Direction.Forwards ? currentIndex + 1 : currentIndex - 1;
-        ReachedEnd = nextIndex >= paths.Length;
-        ReachedStart = nextIndex == 0;
-        if (state == State.PingPong && (ReachedEnd || ReachedStart))
-        {
-            //reverse direction
-            switch (direction)
-            {
-                case Direction.Forwards:
-                    direction = Direction.Backwards;
-                    nextIndex--;
-                    break;
+    //public void CheckPath(Transform other)
+    //{
+    //    Transform currentTransform = GetCurrentTransform();
+    //    int range = (int)Vector2.Distance(currentTransform.position, other.position);
+    //    if (range == 0)
+    //    {
+    //        NextPath();
+    //    }
+    //}
+    //private void NextPath()
+    //{
+    //    int nextIndex = direction == Direction.Forwards ? currentIndex + 1 : currentIndex - 1;
+    //    ReachedEnd = nextIndex >= paths.Length;
+    //    ReachedStart = nextIndex == 0;
+    //    if (state == State.PingPong && (ReachedEnd || ReachedStart))
+    //    {
+    //        //reverse direction
+    //        switch (direction)
+    //        {
+    //            case Direction.Forwards:
+    //                direction = Direction.Backwards;
+    //                nextIndex--;
+    //                break;
 
-                case Direction.Backwards:
-                    direction = Direction.Forwards;
-                    nextIndex++;
-                    break;
-                default:
-                    break;
-            }
-        }
-        else if (state == State.Loop && ReachedEnd)
-        {
-            nextIndex = 0;
-        }
-        else if (state == State.Loop && ReachedStart)
-        {
-            nextIndex = paths.Length - 1;
-        }
-        else if (ReachedStart)
-        {
-            nextIndex = 0;
-        }
-        else if (ReachedEnd)
-        {
-            nextIndex = paths.Length - 1;
-        }
-        currentIndex = nextIndex;
-    }
+    //            case Direction.Backwards:
+    //                direction = Direction.Forwards;
+    //                nextIndex++;
+    //                break;
+    //            default:
+    //                break;
+    //        }
+    //    }
+    //    else if (state == State.Loop && ReachedEnd)
+    //    {
+    //        nextIndex = 0;
+    //    }
+    //    else if (state == State.Loop && ReachedStart)
+    //    {
+    //        nextIndex = paths.Length - 1;
+    //    }
+    //    else if (ReachedStart)
+    //    {
+    //        nextIndex = 0;
+    //    }
+    //    else if (ReachedEnd)
+    //    {
+    //        nextIndex = paths.Length - 1;
+    //    }
+    //    currentIndex = nextIndex;
+    //}
 }
